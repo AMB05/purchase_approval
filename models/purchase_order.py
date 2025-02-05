69cbadfea13f5d7aef1352622d3e96ec4528535d
AMB05 <arrivariva05@gmail.com>
2025-02-06 01:26:07 +0700

    Update Button Confirm to Approval & Cancel

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -5,33 +5,27 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     state = fields.Selection(selection_add=[
-        ("waiting_approval", "Waiting Approval")
-    ], ondelete={"waiting_approval": "set default", "approved": "set default"})
-
+            ("waiting_approval", "Waiting Approval"), 
+            ("approved", "Approved")], 
+            ondelete={"waiting_approval": "set default", "approved": "set default"})
     approved_by = fields.Many2one('res.users', string="Approved By", readonly=True)
 
+    # button confirm
     def button_confirm(self):
-        """Override tombol Confirm Order agar memerlukan approval jika total lebih dari $5000."""
+        # Override Button Confirm Order if amount total >= $5000 waiting_approval
         for order in self:
-            if order.amount_total > 5000:
+            if order.amount_total >= 5000:
                 order.state = "waiting_approval"
             else:
                 super(PurchaseOrder, order).button_confirm()
-
+    
+    # approve manager if status waiting approval
     def action_approve(self):
-        """Approve the purchase order jika dalam status 'Waiting Approval'."""
         for order in self:
             if order.state != "waiting_approval":
                 raise UserError(_("Only orders in 'Waiting Approval' state can be approved."))
             if not self.env.user.has_group('purchase.group_purchase_manager'):
                 raise UserError(_("Only managers can approve this order."))
 
-            order.state = "approved"
+            order.state = "purchase"
             order.approved_by = self.env.user
-
-    def action_force_confirm(self):
-        """Force confirm an approved order."""
-        for order in self:
-            if order.state != "approved":
-                raise UserError(_("Only 'Approved' orders can be confirmed."))
-            super(PurchaseOrder, order).button_confirm()
