e7043c1a3a81d31bcbf077a6a13862ff8efff8f0
AMB05 <arrivariva05@gmail.com>
2025-02-06 01:21:02 +0700

    Update Filter Rest API Category_ID & Category_Name

--- controllers/product_api.py
+++ controllers/product_api.py
@@ -5,8 +5,8 @@ import json
 class ProductAPIController(http.Controller):
 
     @http.route('/api/products', type='http', auth='public', methods=['GET'], csrf=False)
-    def get_products(self, category_id=None, **kwargs):
-        """API untuk mengambil produk dengan filter kategori."""
+    def get_products(self, category_id=None, category_name=None, **kwargs):
+        #GET API Product Filter Category_ID & Category_Nama
         query = """
             SELECT p.id, pt.name, pt.list_price, c.name as category
             FROM product_product p
@@ -14,14 +14,22 @@ class ProductAPIController(http.Controller):
             LEFT JOIN product_category c ON pt.categ_id = c.id
         """
         params = []
-
+        conditions = []
+        # Category_ID
         if category_id:
-            query += " WHERE pt.categ_id = %s"
+            conditions.append("pt.categ_id = %s")
             params.append(int(category_id))
+        # Category_Name
+        if category_name:
+            conditions.append("c.name ILIKE %s")  # Gunakan ILIKE agar case-insensitive
+            params.append(f"%{category_name}%")  # Mendukung pencarian sebagian nama
+        
+        if conditions:
+            query += " WHERE " + " AND ".join(conditions)
 
         request.env.cr.execute(query, tuple(params))
         products = request.env.cr.dictfetchall()
-
+        # return json data
         return request.make_response(
             json.dumps({"status": "success", "products": products}, indent=4),
             headers=[('Content-Type', 'application/json')]
