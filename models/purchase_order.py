8a6567b599dcf4ba0039991db1de67c4404ae807
AMB05 <arrivariva05@gmail.com>
2025-02-05 20:01:18 +0700

    Update Approval Manager

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -6,7 +6,9 @@ class PurchaseOrder(models.Model):
 
     state = fields.Selection(selection_add=[
         ("waiting_approval", "Waiting Approval")
-    ], ondelete={"waiting_approval": "set default"})
+    ], ondelete={"waiting_approval": "set default", "approved": "set default"})
+
+    approved_by = fields.Many2one('res.users', string="Approved By", readonly=True)
 
     def button_confirm(self):
         """Override tombol Confirm Order agar memerlukan approval jika total lebih dari $5000."""
@@ -19,7 +21,17 @@ class PurchaseOrder(models.Model):
     def action_approve(self):
         """Approve the purchase order jika dalam status 'Waiting Approval'."""
         for order in self:
-            if order.state == "waiting_approval":
-                order.state = "purchase"
-            else:
+            if order.state != "waiting_approval":
                 raise UserError(_("Only orders in 'Waiting Approval' state can be approved."))
+            if not self.env.user.has_group('purchase.group_purchase_manager'):
+                raise UserError(_("Only managers can approve this order."))
+
+            order.state = "approved"
+            order.approved_by = self.env.user
+
+    def action_force_confirm(self):
+        """Force confirm an approved order."""
+        for order in self:
+            if order.state != "approved":
+                raise UserError(_("Only 'Approved' orders can be confirmed."))
+            super(PurchaseOrder, order).button_confirm()
